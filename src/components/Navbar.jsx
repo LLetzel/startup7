acb9ecc8fcca237abdc9e70f05970d76d8124e87
floncarovich11 <felipeloncarovichdossantos@gmail.co>
2025-04-23 21:35:05 -0300

    ficou top agora

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,22 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header>
+    <div className="header-wrapper">
       <div className="header">
-      <div className="header-logo">
-        <img src="/src/assets/NewDev.png" alt="" width="140px" />
+        <div className="header-logo">
+          <img src="/src/assets/logo5.png" alt="" width="105px" />
+        </div>
+        <nav>
+          <ul className="nav-links">
+            <li className="link">Home</li>
+            <li className="link">About</li>
+            <li className="link">Services</li>
+            <li className="link">Contact</li>
+            <li className="link">Projects</li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul className="nav-links">
-          <li className="link">Home</li>
-          <li className="link">About</li>
-          <li className="link">Services</li>
-          <li className="link">Contact</li>
-          <li className="link">Projects</li>
-        </ul>
-      </nav>
-      </div>
-      <div className="divisoria"></div>
-    </header>
+    </div>
   );
 };
 
